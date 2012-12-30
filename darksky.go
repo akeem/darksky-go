385a728771596df4e4b11cb8529ea81d61a846c0
Akeem Adeniji <akeem.adeniji@gmail.com>
2012-12-29 19:41:03 -0500

    pushing webRequest into its own method
    A refactor to move web request stuff into its own method

--- darksky.go
+++ darksky.go
@@ -7,8 +7,9 @@ import "io/ioutil"
 const API_KEY = "0bf574e74efefda422944953b78b5df8"
 const API_HOST = "api.darkskyapp.com"
 
-func hourlyForecast(lat ,long string) string {
-  resp, err := http.Get("https://"+API_HOST+"/v1/forecast/"+API_KEY+"/"+lat+","+long+"")
+
+func webRequest(url string) string{
+  resp, err := http.Get(url)
 
   if err != nil {}
 
@@ -16,21 +17,17 @@ func hourlyForecast(lat ,long string) string {
 
   body, err := ioutil.ReadAll(resp.Body)
   return string(body)
+}
+
+func hourlyForecast(lat ,long string) string {
+  return webRequest("https://"+API_HOST+"/v1/forecast/"+API_KEY+"/"+lat+","+long+"")
 }
 
 func interestingStorms()string{
-  resp, err := http.Get("https://"+API_HOST+"/v1/interesting/"+API_KEY+"")
-
-  if err != nil {}
-
-  defer resp.Body.Close()
-
-  body, err := ioutil.ReadAll(resp.Body)
-  return string(body)
+  return webRequest("https://"+API_HOST+"/v1/interesting/"+API_KEY+"")
 }
 
 func main() {
-  //fmt.Println(hourlyForecast("42.7243","-73.6927"))
-  fmt.Println(interestingStorms())
-  fmt.Println("Hello, 世界")
+  fmt.Println(hourlyForecast("42.7243","-73.6927"))
+  //fmt.Println(interestingStorms())
 }
